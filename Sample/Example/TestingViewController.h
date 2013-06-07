a4f78d0a3317298bb345190acf26c1f716ecc5c2
SRubini <srubini@prokarmasoftech.com>
2013-06-07 20:43:02 +0530

    test commit

---
+++ Sample/Example/TestingViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestingViewController.h
+//  Sample
+//
+//  Created by Rubini on 6/7/13.
+//  Copyright (c) 2013 SKANNAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestingViewController : UIViewController
+
+@end
---
+++ TestFolder/PSHomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  PSHomeViewController.m
+//  Sample
+//
+//  Created by Rubini on 5/24/13.
+//  Copyright (c) 2013 SKANNAN. All rights reserved.
+//
+
+#import "PSHomeViewController.h"
+
+@interface PSHomeViewController ()
+ 
+@end
+//Checking by uday
+@implementation PSHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
